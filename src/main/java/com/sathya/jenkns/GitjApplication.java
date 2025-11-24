0ece3edf953b5a4e4a9488b5b88b04b0276e8b00
bazi1630 <shaikbabu9766@gmail.com>
2025-11-24 21:00:56 +0530

    initial commit

---
+++ src/main/java/com/sathya/jenkns/GitjApplication.java
@@ -0,0 +1,13 @@
+package com.sathya.jenkns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitjApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sathya/jenkns/example/Example.java
@@ -0,0 +1,15 @@
+package com.sathya.jenkns.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Example {
+	
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello Spring Boot";
+	}
+	
+
+}
